721f2536f030d8f189fbb39fce1a5ec3587a07be
muhammedajmalkt <muhammedajmalkt@gmail.com>
2025-08-28 17:56:33 +0530

    refactor:cors

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,12 @@ import userRoutes from "./src/routers/userRoutes.js"
 import cookieParser from "cookie-parser"
 
 const app = express()
-app.use(cors({origin:["http://localhost:5173","https://leave-management-ziya.vercel.app"],
-    credentials:true}))
+app.use(cors({
+    origin:["http://localhost:5173","https://leave-management-ziya.vercel.app"],
+    credentials:true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
+}))
 app.use(express.json())
 app.use(cookieParser())
 
