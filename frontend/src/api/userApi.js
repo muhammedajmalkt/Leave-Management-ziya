226a27dbdb8e7a41a01ee9824abec8ae1edb5c47
muhammedajmalkt <muhammedajmalkt@gmail.com>
2025-08-28 17:05:57 +0530

    connectionas

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({   
- baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:7001/api"
-    : "https://leave-management-ziya.onrender.com/api",     withCredentials: true, });
+  baseURL: ["http://localhost:7001/api","https://leave-management-ziya.onrender.com/api"],
+
+         withCredentials: true, });
 
 export const loginUser = (formData) => API.post("/user/login", formData);
